ab9f5a77f9d14b3e92bf985cfcccf36d2990dc4a
dais <daispaysagist@gmail.com>
2019-05-06 17:48:50 +0300

    managed to draw text.

--- LibraryBookStickers.py
+++ LibraryBookStickers.py
@@ -2,52 +2,57 @@ from PIL import Image, ImageDraw, ImageFont
 
 
 class Sticker:
-    def __init__(self, position_in_page=(None, None), author=None, library_location=None, isFiction=False):
+    def __init__(self, position_in_page: tuple = (None, None), author: str = None, library_location: int = None,
+                 is_fiction: bool = False):
         self.position = position_in_page
         self.author = author
         self.location = library_location
-        self.isFiction = isFiction
+        self.is_fiction = is_fiction
 
 
 class StickerSheet:
     A4_dim = (2480, 3508)  # in pixels
     default_sticker_table_dim = (11, 3)  # table cells
 
-    def __init__(self, page_dim=A4_dim, table_dim=default_sticker_table_dim, stickers=[]):
+    def __init__(self, page_dim: tuple = A4_dim, table_dim: tuple = default_sticker_table_dim, stickers: dict = {},
+                 background_color: int = 255,
+                 foreground_color: int = 0):
         self.page_dim = page_dim
         self.table_dim = table_dim
         self.stickers = stickers
+        self.single_sticker_dim = self.__calculate_single_sticker_dim()
+        self.background_color = background_color
+        self.foreground_color = foreground_color
+        self.font = ImageFont.truetype('Pillow/Tests/fonts/FreeMono.ttf', 40)
 
         # create empty white greyscale sheet
-        self.canvas = Image.new(mode='L', size=self.page_dim, color=255)
+        self.canvas = Image.new(mode='L', size=self.page_dim, color=self.background_color)
 
-    def add_sticker(self):
-        pass
+    def __calculate_single_sticker_dim(self):
+        return int(self.page_dim[0] / self.table_dim[0]), int(self.page_dim[1] / self.table_dim[1])
 
-    def remove_sticker(self):
-        pass
+    def add_sticker(self, sticker: Sticker):
+        self.stickers[sticker.position] = sticker
+
+    def remove_sticker(self, sticker: Sticker):
+        self.stickers.pop(sticker.position)
 
     def show_current_stickers(self):
-        pass
+        print(self.stickers)
 
     def save_to_file(self, file_name='Math Library Book Stickers.png', file_format='PNG'):
         # assemble actual image
-        # make a blank image for the text, initialized to transparent text color
-        txt = Image.new('RGBA', base.size, (255, 255, 255, 0))
 
-        # get a font
-        fnt = ImageFont.truetype('Pillow/Tests/fonts/FreeMono.ttf', 40)
-        # get a drawing context
-        d = ImageDraw.Draw(txt)
+        for sticker in self.stickers:
+            # make a blank image for the text
+            sticker_img = Image.new('L', self.single_sticker_dim, color=self.background_color)
 
-        # draw text, half opacity
-        d.text((10, 10), "Hello", font=fnt, fill=(255, 255, 255, 128))
-        # draw text, full opacity
-        d.text((10, 60), "World", font=fnt, fill=(255, 255, 255, 255))
+            # get a drawing context
+            draw = ImageDraw.Draw(sticker_img)
 
-        out = Image.alpha_composite(base, txt)
-
-        out.show()
+            # draw text, half opacity
+            draw.text((0, 0), self.stickers[sticker].author, font=self.font, fill=self.foreground_color)
+            self.canvas.paste(sticker_img)
 
         # save final result to a PNG image file
         self.canvas.save(file_name, file_format)
@@ -55,3 +60,6 @@ class StickerSheet:
 
 if __name__ == '__main__':
     sheet = StickerSheet()
+    my_sticker = Sticker((0, 0), 'ABC', 0, is_fiction=False)
+    sheet.add_sticker(my_sticker)
+    sheet.save_to_file()
